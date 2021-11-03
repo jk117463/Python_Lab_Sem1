72383e411af227ce938f95ad7764c3f5d1ab0d35
jk117463 <92452727+jk117463@users.noreply.github.com>
2021-11-03 14:57:20 +0100

    added whitespaces

--- main.py
+++ main.py
@@ -11,11 +11,10 @@ def print_hi(name):
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-
     print_hi('PyCharm')
-    a=10
-    b=20
-    c=a+b
+    a = 10
+    b = 20
+    c = a + b
     print(c)
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
