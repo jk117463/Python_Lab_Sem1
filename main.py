d6c0571a30bb1a43a35edf09e1639dd47291c5ea
jk117463 <92452727+jk117463@users.noreply.github.com>
2021-11-03 14:52:36 +0100

    addition

--- main.py
+++ main.py
@@ -13,5 +13,9 @@ def print_hi(name):
 if __name__ == '__main__':
 
     print_hi('PyCharm')
+    a=10
+    b=20
+    c=a+b
+    print(c)
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
