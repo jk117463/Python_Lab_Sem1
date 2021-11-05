a0bac348a0890bdaf10b3111098324532cdbb446
jk117463 <92452727+jk117463@users.noreply.github.com>
2021-11-05 14:25:41 +0100

    added new file, creates txt file

---
+++ CreateTXTfile.py
@@ -0,0 +1,9 @@
+for i in range(1, 101):  # Range
+    fn = "A" + f"{i:04d}" + ".txt"  # set Filename
+    print(fn)
+    pn = "C:\\Users\\jathi\\PycharmProjects\\Python_Lab_Sem1\\CC\\homework2\\" + fn  # Set Pathname depends on ur system
+    fh = open(pn, 'w')  # Create file and open
+    fh.write("jk117463")  # Write student id to created file
+    fh.close()  # Close file
+
+
