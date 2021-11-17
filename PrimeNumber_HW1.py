78d2ba65633ccd2300439f6ee1e0b702e68c5704
jk117463 <92452727+jk117463@users.noreply.github.com>
2021-11-17 15:55:52 +0100

    Added print Logic and comments

--- PrimeNumber_HW1.py
+++ PrimeNumber_HW1.py
@@ -1,15 +1,15 @@
-print("Enter the Limit untill which Prime numbers needs to be listed")
+print("Enter the Limit untill which Prime numbers needs to be listed  : ")
 limit = int(input())
 
-print(limit)
+print(" List of prime numbers till %s are " % limit)
 
-i = 1
-ind = 0
-j = 1
-for i in range(i, limit):
-    for j in range (i, int(i/2)):
-        if i%j == 0:
+for i in range(2, limit):
+    ind = 0 # Indicator variable set value as 0
+    j = 2
+    while j <= int(i/2): # To continuosly divide by consecutive number from 2 till half of its value
+        if i % j == 0: # If remainder is 0, that means it is not prime number, so setting indicator to 1, ignoring rest of possible divisions  and breaking onto upper loop
             ind = 1
             break
-    if ind == 1:
+        j = j + 1
+    if ind == 0: #If indicator is still 0, that means it is a prime number
         print(i)
