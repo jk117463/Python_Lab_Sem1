b20e461b3c5871f36c68d3508b8ae6ea751ac29c
jk117463 <92452727+jk117463@users.noreply.github.com>
2021-12-15 15:00:08 +0100

    Count size of a directory

--- space_count.py
+++ space_count.py
@@ -1,14 +1,16 @@
+#Count size of a folder including sub-directory
 import os
-my_dir = 'C:\\Datascience\\Temp'
+my_dir = 'C:\\Datascience\\Sem 1\\PythonLab\\temp'
 
 def sum_space(folder):
     sum = 0
     for f in os.listdir(folder):
-        if os.path.isfile(os.path.join(folder, f)):
-            size = os.path.getsize(os.path.join(folder, f))
+        path = os.path.join(folder, f)
+        if os.path.isfile(path):
+            size = os.path.getsize(path)
             sum = sum + size
         else:
-            sum = sum + sum_space(os.path.join(folder, f))
+            sum = sum + sum_space(path) # Call recursively for all sub folders
     return sum
 
 print('Space occupied by {}: {}'.format(my_dir, sum_space(my_dir)))
