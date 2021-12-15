b73005981056776538f21f97d1ee15c895034958
jk117463 <92452727+jk117463@users.noreply.github.com>
2021-12-15 14:05:48 +0100

    comments

--- Bowls.py
+++ Bowls.py
@@ -1,19 +1,18 @@
-# #using loop
-print("Enter the rows  : ")  # Requesting user input
-limit = input()
-Bowl = 0
-for i in range (int(limit)+1):
-    Bowl = Bowl + i
-print("Bowls :", Bowl)
+# # #using loop
+# print("Enter the rows  : ")  # Requesting user input
+# limit = input()
+# Bowl = 0
+# for i in range (int(limit)+1):
+#     Bowl = Bowl + i
+# print("Bowls :", Bowl)
 
 
 #using recursive function
 def tri_recursion(k):
-  if(k>0):
+  if(k>1):
     result = k+tri_recursion(k-1)
-    print(result)
   else:
-    result = 0
+    result = 1
   return result
 
 print("Enter the rows  : ")  # Requesting user input
@@ -21,12 +20,12 @@ limit = input()
 print("\n\nRecursion Example Results")
 print(tri_recursion(int(limit)))
 
-
-#using sequence
-def bowls_sequence(n):
-    sum = n/2 * (2 + (n-1)*1)
-    return int(sum)
-print("Enter the rows  : ")  # Requesting user input
-limit = input()
-bowls = bowls_sequence(int(limit))
-print('Bowls needed {}'.format(bowls))
+#
+# #using sequence
+# def bowls_sequence(n):
+#     sum = n/2 * (2 + (n-1)*1)
+#     return int(sum)
+# print("Enter the rows  : ")  # Requesting user input
+# limit = input()
+# bowls = bowls_sequence(int(limit))
+# print('Bowls needed {}'.format(bowls))
